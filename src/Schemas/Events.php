0865e4eb2270c71d80af1c861bad202113a58616
JJJ <88951+JJJ@users.noreply.github.com>
2026-07-17 08:31:35 +0000

    Regenerate schemas from Sugar Calendar 3.12.0

--- src/Schemas/Events.php
+++ src/Schemas/Events.php
@@ -23,26 +23,27 @@ class Events extends Schema {
 
 	/** @var array<int, array<string, mixed>> */
 	public $columns = array(
-			array( 'name' => 'id', 'type' => 'bigint', 'length' => '20', 'unsigned' => true, 'extra' => 'auto_increment', 'primary' => true ),
-			array( 'name' => 'object_id', 'type' => 'bigint', 'length' => '20', 'unsigned' => true, 'default' => '0' ),
+			array( 'name' => 'id', 'type' => 'bigint', 'unsigned' => true, 'extra' => 'auto_increment', 'primary' => true ),
+			array( 'name' => 'object_id', 'type' => 'bigint', 'unsigned' => true, 'default' => '0' ),
 			array( 'name' => 'object_type', 'type' => 'varchar', 'length' => '20', 'default' => '' ),
 			array( 'name' => 'object_subtype', 'type' => 'varchar', 'length' => '20', 'default' => '' ),
 			array( 'name' => 'title', 'type' => 'text' ),
 			array( 'name' => 'content', 'type' => 'longtext' ),
 			array( 'name' => 'status', 'type' => 'varchar', 'length' => '20', 'default' => '' ),
 			array( 'name' => 'start', 'type' => 'datetime', 'default' => '0000-00-00 00:00:00' ),
-			array( 'name' => 'start_tz', 'type' => 'varchar', 'length' => '20', 'default' => '' ),
+			array( 'name' => 'start_tz', 'type' => 'varchar', 'length' => '155', 'default' => '' ),
 			array( 'name' => 'end', 'type' => 'datetime', 'default' => '0000-00-00 00:00:00' ),
-			array( 'name' => 'end_tz', 'type' => 'varchar', 'length' => '20', 'default' => '' ),
+			array( 'name' => 'end_tz', 'type' => 'varchar', 'length' => '155', 'default' => '' ),
 			array( 'name' => 'all_day', 'type' => 'tinyint', 'length' => '1', 'unsigned' => false, 'default' => '0' ),
 			array( 'name' => 'recurrence', 'type' => 'varchar', 'length' => '20', 'default' => '' ),
-			array( 'name' => 'recurrence_interval', 'type' => 'bigint', 'length' => '20', 'unsigned' => true, 'default' => '0' ),
-			array( 'name' => 'recurrence_count', 'type' => 'bigint', 'length' => '20', 'unsigned' => true, 'default' => '0' ),
+			array( 'name' => 'recurrence_interval', 'type' => 'bigint', 'unsigned' => true, 'default' => '0' ),
+			array( 'name' => 'recurrence_count', 'type' => 'bigint', 'unsigned' => true, 'default' => '0' ),
 			array( 'name' => 'recurrence_end', 'type' => 'datetime', 'default' => '0000-00-00 00:00:00' ),
 			array( 'name' => 'recurrence_end_tz', 'type' => 'varchar', 'length' => '20', 'default' => '' ),
 			array( 'name' => 'date_created', 'type' => 'datetime', 'default' => '0000-00-00 00:00:00' ),
 			array( 'name' => 'date_modified', 'type' => 'datetime', 'default' => '0000-00-00 00:00:00' ),
 			array( 'name' => 'uuid', 'type' => 'varchar', 'length' => '100', 'default' => '' ),
+			array( 'name' => 'venue_id', 'type' => 'bigint', 'unsigned' => true, 'default' => '0' ),
 	);
 
 	/** @var array<int, array<string, mixed>> */
@@ -50,7 +51,8 @@ class Events extends Schema {
 			array( 'type' => 'key', 'name' => 'event_recur', 'columns' => array( 'recurrence' ) ),
 			array( 'type' => 'key', 'name' => 'event_recur_times', 'columns' => array( 'recurrence_end', 'recurrence_end_tz' ) ),
 			array( 'type' => 'key', 'name' => 'event_status', 'columns' => array( 'status' ) ),
-			array( 'type' => 'key', 'name' => 'event_times', 'columns' => array( 'start', 'end', 'start_tz', 'end_tz' ) ),
+			array( 'type' => 'key', 'name' => 'event_times', 'columns' => array( 'start', 'end', 'start_tz(50)', 'end_tz(50)' ) ),
+			array( 'type' => 'key', 'name' => 'event_venue', 'columns' => array( 'venue_id' ) ),
 			array( 'type' => 'key', 'name' => 'object', 'columns' => array( 'object_id', 'object_type', 'object_subtype' ) ),
 			array( 'type' => 'primary', 'columns' => array( 'id' ) ),
 	);
